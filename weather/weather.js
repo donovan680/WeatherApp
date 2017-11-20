af1ce9857586c8921a9a77a38d6c3b4dd998a715
niharrs <niharika.3297@gmail.com>
2017-11-20 17:54:34 +0530

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+  request({
+    url: `https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/${lat},${lng}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Forecast.io server.');
+    } else if (response.statusCode === 400) {
+      callback('Unable to fetch weather.');
+    } else if (response.statusCode === 200) {
+      callback(undefined, {
+        temperature: (body.currently.temperature-32)*0.5,
+        apparentTemperature: (body.currently.apparentTemperature-32)*0.5
+      });
+    }
+  });
+};
+
+module.exports.getWeather = getWeather;
